6fadb8db7b3d4e26a5ff4df3dd9cd7c073cdcff7
fanzhouyi <fanzhouyi@esto.cn>
2019-12-30 08:38:58 +0800

    first commit

---
+++ src/main/java/stream/GroupByTest.java
@@ -0,0 +1,14 @@
+package stream;/**
+ * Created by Zhouyi.Fan on 2019/12/30.
+ *
+ * @version 版本号:1.01
+ */
+
+/**
+ * DESCRIPTION: learn grouping by handle and for me think and review
+ **/
+public class GroupByTest {
+    public static void main(String[] args) {
+        System.out.println("good");
+    }
+}
